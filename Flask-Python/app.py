eb5e57d65e55c6e08aebd7e31d9c1965d4f6113e
zhd5120153951 <zenghedong@outlook.com>
2023-11-28 19:56:18 +0800

    重构登陆功能
    Signed-off-by: zhd5120153951 <zenghedong@outlook.com>

--- Flask-Python/app.py
+++ Flask-Python/app.py
@@ -1,28 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, Response, jsonify
+from matplotlib.backend_bases import cursors
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import logging
 import cv2
 import psutil
 from sympy import threaded, true
 
-
-# 配置基本日志设置
-logging.basicConfig(
-    level=logging.DEBUG,  # 设置日志级别，可以选择DEBUG, INFO, WARNING, ERROR, CRITICAL
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # 设置日志消息的格式
-    datefmt='%Y-%m-%d %H:%M:%S',  # 设置日期时间格式
-    filename='./App.log',  # 指定日志输出到文件
-    filemode='w'  # 指定文件写入模式（a表示追加，w表示覆盖）
-)
-# 创建一个日志记录器
-logger = logging.getLogger('my_logger')
 # 创建网页应用对象
 app = Flask(__name__)
-app.secret_key = '@ZHDzhd2231'
-# 使用OpenCV捕获RTSP流
-rtsp_url = "rtsp://admin:jiankong123@192.168.23.10:554/Streaming/Channels/101"
-cap = cv2.VideoCapture(rtsp_url)
-
+# app.config["secret_key"] = 'daito_yolov5_flask'
 # Function to connect to the SQLite database
 
 
@@ -42,34 +29,54 @@ def init_db():
         connection.commit()
         connection.close()
 
+# 创建数据库表
+
+
+def create_table():
+    with sqlite3.connect("users.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+            );''')
+
 # 路由--first page
 
 
 @app.route('/')
 def home():
+    init_db()
     return render_template('login.html')
 
 # 路由--login page
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
 
-    connection = get_db_connection()
-    isExistUser = connection.execute(
-        'SELECT * FROM users WHERE username = ? AND password = ?', (username, password)).fetchone()
-    connection.close()
+    with sqlite3.connect("users.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            'SELECT * FROM users WHERE username = ?', (username, ))
+        isExistUser = cursor.fetchone()
 
-    if isExistUser:
-        flash('Login successful', 'success')
+    if isExistUser and check_password_hash(isExistUser[2], password):
+        # flash('Login successful', 'success')
         return redirect(url_for('homepage'))  # redirect()是某个页面的路由函数
 
-    else:
-        flash('Invalid username or password', 'error')
-        return redirect(url_for('home'))
+    # if isExistUser:
+    #     flash('Login successful', 'success')
+    #     return redirect(url_for('homepage'))  # redirect()是某个页面的路由函数
 
+    # else:
+    #     flash('Invalid username or password', 'error')
+    #     return redirect(url_for('home'))
+    # flash('Invalid username or password', 'error')
+    return redirect(url_for('home'))
 # Route for the registration page
 
 
@@ -78,14 +85,16 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
 
-        connection = get_db_connection()
-        connection.execute(
-            'INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
-        connection.commit()
-        connection.close()
-
-        flash('Registration successful. You can now log in.', 'success')
+        with sqlite3.connect("users.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                "INSERT INTO users (username,password) VALUES (?,?)", (
+                    username, hashed_password)
+            )
+            conn.commit()
+        # flash('Registration successful. You can now log in.', 'success')
         return redirect(url_for('home'))
 
     return render_template('register.html')
@@ -159,5 +168,26 @@ def set_ip():
 
 
 if __name__ == '__main__':
+    # 配置基本日志设置
+    logging.basicConfig(
+        level=logging.DEBUG,  # 设置日志级别，可以选择DEBUG, INFO, WARNING, ERROR, CRITICAL
+        # 设置日志消息的格式
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',  # 设置日期时间格式
+        filename='./App.log',  # 指定日志输出到文件
+        filemode='w'  # 指定文件写入模式（a表示追加，w表示覆盖）
+    )
+    # 创建一个日志记录器
+    logger = logging.getLogger('my_logger')
+
+    # 使用OpenCV捕获RTSP流
+    # rtsp_url = "rtsp://admin:jiankong123@192.168.23.10:554/Streaming/Channels/101"
+    rtsp_url = 0
+    cap = cv2.VideoCapture(rtsp_url)
+
+    create_table()
+    # 配置密钥,用于加密session数据
+    app.config['SECREAT_KEY'] = "daito_yolov5_flask"
+
     app.run(host="0.0.0.0", debug=True)
     # app.run(host="0.0.0.0", debug=True)
