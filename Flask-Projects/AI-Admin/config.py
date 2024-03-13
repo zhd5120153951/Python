575a528de97357353c284adcadf301165ddf6f5f
zhd5120153951 <zenghedong@outlook.com>
2024-03-13 10:28:19 +0800

    正心全栈
    Signed-off-by: zhd5120153951 <zenghedong@outlook.com>

--- Flask-Projects/AI-Admin/config.py
+++ Flask-Projects/AI-Admin/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_admin.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_admin.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_TOKEN_LOCATION = ["headers", "cookies", "json", "query_string"]
--- Flask-Projects/AI-Admin/readme.md
+++ Flask-Projects/AI-Admin/readme.md
@@ -3,3 +3,4 @@ flask db
 flask db init
 flask db migrate -m 'init'
 flask db upgrade
+运行:flask run or python app.py(这个方式必须要把端口设为flask一致)
